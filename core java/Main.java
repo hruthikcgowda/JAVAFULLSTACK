f95df1bae55b99a4ed17ba7c13074f19386dc100
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-05-18 22:02:50 +0530

    Concept of encapsulation

---
+++ core java/Main.java
@@ -0,0 +1,22 @@
+class Balance{
+    private int bal;
+
+    public void setBal(int data){
+        if(data < 0){
+            System.out.print("Invalid num");
+        }else{
+            bal=data;
+        }
+    }
+    public int getBal(){
+        return bal;
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Balance sbi = new Balance();
+
+        sbi.setBal(-10000);
+        System.out.print(sbi.getBal());
+    }
+}

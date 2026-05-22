856658329ecb51369ed9bc0bd63c1654282541a3
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-05-22 11:37:01 +0530

    about static

---
+++ core java/Day19.java
@@ -0,0 +1,17 @@
+public class Day19{
+    static
+    {
+        System.out.println("Java");
+    }
+    static
+    {
+        System.out.println("C");
+    }
+    public static void main(String[] args){
+        System.out.println("Python");
+    }
+     static
+    {
+        System.out.println("C++");
+    }
+}
